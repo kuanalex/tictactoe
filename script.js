10ddc66b6b85661d47e370459aacce298a063cb0
Alex <kuanalex96@gmail.com>
2021-05-20 15:15:16 -0400

    get sign, set field, get field, reset gameboard

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ const Player = (sign) => {
         getSign
     };
 };
+
+const gameBoard = (() => {
+    const board = ["", "", "", "", "", "", "", "", ""];
+
+    const setField = (index, sign) => {
+        if (index > board.length) return;
+        board[index] = sign;
+    };
+
+    const getField = (index) => {
+        if (index > board.length) return;
+        return board[index];
+    };
+
+    const reset = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = "";
+        }
+    };
+
+    return {
+        setField,
+        getField,
+        reset
+    };
+})();
